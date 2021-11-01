7e2b24569fa8d91c5f5623c3cff0384153343654
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2021-11-01 15:34:04 -0400

    added style sheet and minor styling to layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from "../pages/layout";
 const IndexPage = ()=> {
   return(
     <div>
-      <h1>Developer Blog</h1>
+      {/* <h1>Developer Blog</h1> */}
       <Layout pageTitle="Home Page">
         <p>I'm making this blog to exercise and document working with React.</p>
       </Layout>
